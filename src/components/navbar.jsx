5ec77da042983dd1a46efd5659c00eed50ad23a6
Caspar2318 <108163945+Caspar2318@users.noreply.github.com>
2023-01-31 20:24:41 +1000

    add all

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,36 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
+import "./navbar.css";
 
-export default function Navbar() {
-  const styles = {
-    navbar: {
-      width: "100%",
-      height: "80px",
-      display: "flex",
-      justifyContent: "flex-end",
-      alignItems: "center",
-      background: "black",
-    },
-    area: { marginRight: "50px", display: "flex", alignItems: "center" },
-    link: {
-      textDecoration: "none",
-      color: "white",
-      marginLeft: "20px",
-      fontSize: "25px",
-    },
-  };
-
+export const Navbar = () => {
   return (
-    <div className="navbar" style={styles.navbar}>
-      <div className="links" style={styles.area}>
-        <Link to="/" style={styles.link}>
-          Shop
-        </Link>
-        <Link to="/cart" style={styles.link}>
+    <div className="navbar">
+      <div className="links">
+        <Link to="/"> Shop </Link>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/cart">
           <ShoppingCart size={32} />
         </Link>
       </div>
     </div>
   );
-}
+};
